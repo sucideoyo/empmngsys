5bcb51febbaf6029fc615cda915f34daaf61a231
shivam <oyo@administrators-MacBook-Pro-3.local>
2018-01-08 14:28:56 +0530

    initial setup

---
+++ src/main/java/com/oyorooms/intern/bootstrap/empmngsys/EmpmngsysApplication.java
@@ -0,0 +1,12 @@
+package com.oyorooms.intern.bootstrap.empmngsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmngsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmngsysApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oyorooms/intern/bootstrap/empmngsys/EmpmngsysApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oyorooms.intern.bootstrap.empmngsys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmpmngsysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
